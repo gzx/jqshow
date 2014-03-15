44a4a3ffe5db61b4f2cd04c9c12ea462b0698e99
cheney <wancj17@gmail.com>
2014-03-15 15:51:23 +0800

    feat: add page lecture

---
+++ lecture.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            意大利JoynCleon婧麒防辐射服官网,专业防辐射孕妇装供应商
+        </title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="Keywords" content="婧麒孕妇防辐射服,防辐射服,防辐射孕妇装,婧麒官网,婧麒旗舰店,孕妇装,婧麒官方网站,婧麒网,婧麒防辐射服,意大利婧麒防辐射服">
+        <meta name="Description" content="意大利婧麒全球防辐射服领先品牌，专注于健康环保服饰领域，独家研发深海沉碇防辐射面料，产品防辐射效果超强;她简约大方的风格在服饰界可谓独树一帜，深受广大消费者喜爱！秉承“因爱而生”的品牌理念，专为年轻个性、热爱生活、健康自信的女性而设计。">
+
+        <link rel="stylesheet" type="text/css" href="./css/index.css"/>
+
+        <script src="http://libs.baidu.com/jquery/1.10.2/jquery.min.js" type="text/javascript"></script>
+        <script src="./js/layout.js" type="text/javascript"></script>
+
+        <style type="text/css">
+
+            .wrap {
+                background: #f1edee;
+            }
+            #contentsBanner {
+                width: 100%;
+                height: 379px;
+                background: url(../images/lecture/bg.jpg)  #e3dfd6 no-repeat center top;
+            }
+            #ContentWrap {
+                margin: 0 auto;
+                padding-bottom: 200px;
+                width: 960px;
+                background: #fcfbfc;
+                text-align: center;
+                color: #584674;
+            }
+            #ContentWrap p {
+                margin: 45px auto 8px auto;
+                font-size: 18px;
+            }
+            #ContentWrap a {
+                font-size: 22px;
+                color: #584674;
+            }
+
+        </style>
+
+    </head>
+    
+    <body>
+        <!-- header -->
+        <?php include("inc_head.php");?>
+        <!-- //header -->
+
+        <div class="wrap">
+            <div id="contentsBanner">
+                <h2 class="visuallyhidden">活动</h2>
+            </div>
+
+            <div id="ContentWrap">
+                <img src="../images/lecture/pic01.jpg" style="margin-top: 80px">
+                <img src="../images/lecture/pic02.jpg" style="margin-top: 100px">
+                <img src="../images/lecture/pic03.jpg" style="margin-top: 100px">
+                <img src="../images/lecture/pic04.jpg" style="margin-top: 115px">
+                <img src="../images/lecture/pic05.jpg" style="margin-top: 110px">
+                <img src="../images/lecture/pic06.jpg" style="margin-top: 130px">
+                <img src="../images/lecture/pic07.jpg" style="margin-top: 115px">
+                <img src="../images/lecture/qr.jpg" style="margin-top: 90px">
+                <p>关注婧麒防辐射，做健康准妈咪！</p>
+                <a href="" target="_blank">进入婧麒店铺选购>></a>
+            </div>
+        </div>
+
+        <!-- footer -->
+        <?php include("inc_foot.php");?>
+
+        <!-- //footer -->
+    </body>
+
+</html>
