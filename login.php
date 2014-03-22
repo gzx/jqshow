eeeb06df53d3715fb171075654d5ba02d5d4045f
cheney <wancj17@gmail.com>
2014-03-22 13:11:59 +0800

    add login page

---
+++ login.php
@@ -0,0 +1,290 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>
+            意大利JoynCleon婧麒防辐射服官网,专业防辐射孕妇装供应商
+        </title>
+        <meta name="Keywords" content="婧麒孕妇防辐射服,防辐射服,防辐射孕妇装,婧麒官网,婧麒旗舰店,孕妇装,婧麒官方网站,婧麒网,婧麒防辐射服,意大利婧麒防辐射服">
+        <meta name="Description" content="意大利婧麒全球防辐射服领先品牌，专注于健康环保服饰领域，独家研发深海沉碇防辐射面料，产品防辐射效果超强;她简约大方的风格在服饰界可谓独树一帜，深受广大消费者喜爱！秉承“因爱而生”的品牌理念，专为年轻个性、热爱生活、健康自信的女性而设计。">
+        <?php include("inc_mini_head.php");?>
+
+        <style type="text/css">
+
+            .ui-form-item-20pd {
+                padding-bottom: 18px;
+            }
+            .ui-form-item-30pd {
+                padding-bottom: 28px;
+            }
+
+            .ui-form-item input {
+                vertical-align: middle;
+            }
+
+            .ui-button {
+                display: block;
+                text-align: center;
+                text-decoration: none;
+                vertical-align: middle;
+                cursor: pointer;
+                width: 219px;
+                padding: 0 20px;
+                font-size: 16px;
+                line-height: 40px;
+                height: 40px;
+                color: #FFF;
+                text-shadow: 1px 1px 0 rgba(0, 0, 0, 0.2);
+                font-weight: 400;
+                font-weight: bold\0;
+                _font-weight: 700;
+                background-color: #E66E1E;
+                background: -webkit-gradient(linear,left top,left bottom,from(#FFA800),to(#E66E1E));
+                background: -moz-linear-gradient(top,#ffa800,#e66e1e);
+                filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#ffa800', endColorstr='#e66e1e');
+                background: -o-linear-gradient(top,#FFA600,#FE9200);
+                background: -ms-linear-gradient(top,#ffa800 0,#e66e1e 100%);
+                background: linear-gradient(top,#ffa800,#e66e1e);
+                border: 0;
+                -webkit-border-radius: 0;
+                -moz-border-radius: 0;
+                border-radius: 0;
+            }
+            .login-box {
+                position: relative;
+                margin: 0 auto;
+                width: 960px;
+            }
+
+            .login-modern .ui-form-title {
+                color: #FFF;
+            }
+
+            .ui-form-title {
+                font: 18px/28px tahoma,arial,"Hiragino Sans GB","Microsoft Yahei",\5b8b\4f53;
+                font-weight: 500;
+                font-weight: bold\0;
+                padding: 26px 0 16px;
+                width: 100%;
+                color: #4D4D4D;
+            }
+
+            .login-box a {
+                color: #FFF;
+                opacity: .6;
+            }
+
+            .login-modern {
+                position: absolute;
+                right: 0;
+                top: 100px;
+                width: 286px;
+                height: 387px;
+                border: 1px solid rgba(0, 0, 0, 0);
+                background: rgba(0, 0, 0, 0.4);
+                _background: 0;
+                _border: 0;
+            }
+            #login {
+                width: 220px;
+                padding: 0 31px 0 33px;
+                position: relative;
+                overflow: hidden;
+            }
+
+            .login .ui-label {
+                border-top: 1px solid #A7A7A7;
+                border-bottom: 1px solid #A7A7A7;
+                border-left: 1px solid #A7A7A7;
+            }
+            .ui-label {
+                width: 39px;
+                height: 38px;
+                display: block;
+                float: left;
+                cursor: pointer;
+            }
+            .ui-form {
+                font-size: 12px;
+                line-height: 1.5;
+                zoom: 1;
+            }
+
+            .ui-form-item {
+                padding: 0 0 10px;
+                zoom: 1;
+                position: relative;
+            }
+
+            .ui-form-item:after {
+                clear: both;
+                content: '.';
+                display: block;
+                height: 0;
+                visibility: hidden;
+            }
+
+            .login-modern .ui-input {
+                border: 1px solid #FFF;
+            }
+            .ui-form-item input {
+                vertical-align: middle;
+            }
+            .ui-input-normal, .standardPwdContainer .ui-input, .alieditContainer .ui-input {
+                width: 159px;
+                _width: 157px;
+            }
+
+            .ui-input {
+                float: left;
+                position: relative;
+                padding: 9px;
+                line-height: 20px;
+                height: 20px;
+                color: #595959;
+                font-size: 12px;
+                vertical-align: middle;
+                -webkit-border-radius: 0;
+                -moz-border-radius: 0;
+                border-radius: 0;
+                box-shadow: none;
+            }
+
+            .ui-checkcode {
+                width: 80px;
+                height: 40px;
+                float: left;
+                margin-left: 10px;
+                position: relative;
+                background: #FFF;
+                cursor: pointer;
+                overflow: hidden;
+                zoom: 1;
+            }
+
+            .ui-input-checkcode {
+                width: 69px;
+            }
+
+            .ui-label .ui-icon {
+                width: 39px;
+                height: 38px;
+                display: block;
+                text-indent: -9999em;
+                overflow: hidden;
+                background-color: #AAABAB;
+                background-image: url(https://i.alipayobjects.com/e/201311/1Ybqf7665d.png);
+                _background-image: url(https://i.alipayobjects.com/e/201311/1Yc1UNOrYL.png);
+                background-repeat: no-repeat;
+            }
+
+            .login .ui-label:hover {
+                border-top: 1px solid #6D6D6D;
+                border-bottom: 1px solid #6D6D6D;
+                border-left: 1px solid #6D6D6D;
+            }
+
+            .ui-label:hover .ui-icon, .ui-label-hover .ui-icon {
+                cursor: pointer;
+                background-color: #6D6D6D;
+            }
+
+            .ui-icon-userDEF {
+                background-position: -2px 5px;
+            }
+
+            .ui-icon-securityOFF {
+                background-position: -84px 5px;
+            }
+
+            .ui-icon-checkcodeT {
+                background-position: -126px 5px;
+            }
+
+            .ui-form-other {
+                position: relative;
+                line-height: 18px;
+                padding-top: 7px;
+                height: 18px;
+                zoom: 1;
+                _width: 100%;
+                _overflow: hidden;
+            }
+
+        </style>
+
+    </head>
+    
+    <body>
+        <!-- header -->
+        <?php include("inc_head.php");?>
+        <!-- //header -->
+
+        <div id="visual">
+            <div class="visualRote">
+                <ul>
+                    <li class="image01"></li>
+                    <li class="image02"></li>
+                    <li class="image03"></li>
+
+                </ul>
+            </div>
+            <div class="login-box">
+                <div class="login login-modern" id="J-login">
+                    <form name="loginForm" id="login">
+                        <h2 class="ui-form-title " id="J-login-title">登录</h2>
+
+                        <div class="ui-form-item" id="J-username">
+                            <label id="J-label-user" class="ui-label" seed="authcenter-switch-account">
+                                <span class="ui-icon ui-icon-userDEF">账户名：</span>
+                            </label>
+                            <input type="text" id="J-input-user" class="ui-input ui-input-normal" name="logonId" autocomplete="off" tabindex="1" value="" maxlength="100" placeholder="邮箱地址/手机号码" >
+                        </div>
+
+                        <div class="ui-form-item ui-form-item-20pd" id="J-password">
+                            <label id="J-label-editer" class="ui-label" data-desc="登录密码">
+                                <span class="ui-icon ui-icon-securityOFF" id="safeSignCheck">登录密码</span>
+                            </label>
+                                
+                            <span class="standardPwdContainer" style="display: inline-block;">
+                              <input type="password" tabindex="2" id="password_input" name="password_input" class="ui-input" oncontextmenu="return false" onpaste="return false" oncopy="return false" oncut="return false" autocomplete="off" >
+                            </span>
+
+                            <p class="ui-form-other ui-form-other-fg">
+                                <a class="textlink forgot" href="javascript:" tabindex="5" seed="authcenter-pwd-forget" target="_blank">忘记登录密码？</a>
+                            </p>
+                        </div>
+
+                        <div id="J-checkcode" class="ui-form-item">
+                            <label id="J-label-checkcode" class="ui-label">
+                                <span class="ui-icon ui-icon-checkcodeT" id="J-switchCheckcode">校验码</span>
+                            </label>
+                            <input type="text" placeholder="验证码" value="" class="ui-input ui-input-checkcode" id="J-input-checkcode" data-type="IMAGE" maxlength="4" name="checkCode" autocomplete="off" tabindex="3" >
+                            <input type="hidden" name="idPrefix" value="">
+                            <span class="sl-checkcode" id="J-checkcodeIcon"></span>
+                            <div class="ui-checkcode">
+                                    <img class="ui-checkcode-img" id="J-checkcode-img" src="https://omeo.alipay.com/service/checkcode?sessionID=1a1ae6e73f7e9f4ca9f42a3687e48ba4&amp;t=0.8593563253975735" title="点击图片刷新验证码" alt="点击图片刷新验证码" seed="authcenter-img-checkcode">
+                                    <div class="ui-checkcode-voice fn-hide" id="J-checkcode-voice" data-src="https://omeo.alipay.com/service/audiocode?sessionID=1a1ae6e73f7e9f4ca9f42a3687e48ba4&amp;r=0.17731386053328824" seed="authcenter-voice-checkcode">
+                                        <span class="sl-process" id="J-checkcode-voice-bg"></span>
+                                    </div>
+                                </div>
+                        </div>
+
+                        <div class="ui-form-item ui-form-item-30pd" id="J-submit">
+                            <input type="submit" value="登 录" class="ui-button" id="J-login-btn" tabindex="4">
+                            <p class="ui-form-other">
+                                <a href="https://login.taobao.com/member/login.jhtml?style=alipay&amp;goto=https%3A%2F%2Flab.alipay.com%2Fuser%2Fnavigate.htm%3Fsign_from%3D3000" class="textlink" title="淘宝会员登录" tabindex="6" target="_blank">登录</a>
+                                <a class="register" href="https://lab.alipay.com/user/reg/index.htm" target="_blank" title="免费注册" tabindex="8">注册</a>
+                            </p>
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+        <!-- footer -->
+        <?php include("inc_foot.php");?>
+        <!-- //footer -->
+    </body>
+
+</html>
